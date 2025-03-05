cff92ac139208710c8c3068e674341b4300b5330
Silvio <Silvio.Habel@gmail.com>
2025-03-05 22:40:54 +0100

    Changelog-Funktion hinzugefügt
    - Neue Route und Controller-Methode für Changelog-Seite implementiert
    - Git-Commit-Verlauf wird dynamisch abgerufen und gruppiert
    - Changelog-Link im Hauptmenü hinzugefügt
    - Kleine Verbesserung der Übersetzung für Fotoupload-Hinweis

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -41,4 +41,48 @@ class IndexController extends FinderAbstractController
         return $this->render('index/contact.html.twig');
     }
 
+    #[Route('/changelog', name: 'app_changelog')]
+    public function changelog(): Response
+    {
+        // Hole die Git-Commits
+        $commits = [];
+        $gitLog = [];
+        exec('git log --pretty=format:"%ad|%s" --date=format:"%Y-%m-%d" -n 50', $gitLog);
+        
+        foreach ($gitLog as $log) {
+            $parts = explode('|', $log);
+            if (count($parts) === 2) {
+                $date = \DateTime::createFromFormat('Y-m-d', $parts[0]);
+                $commits[] = [
+                    'date' => $date->format('d.m.Y'),
+                    'sortDate' => $parts[0], // Original-Datum für Sortierung
+                    'message' => $parts[1]
+                ];
+            }
+        }
+
+        // Gruppiere Commits nach Datum
+        $groupedCommits = [];
+        foreach ($commits as $commit) {
+            $date = $commit['date'];
+            $sortDate = $commit['sortDate'];
+            if (!isset($groupedCommits[$sortDate])) {
+                $groupedCommits[$sortDate] = [
+                    'displayDate' => $date,
+                    'commits' => []
+                ];
+            }
+            $groupedCommits[$sortDate]['commits'][] = [
+                'message' => $commit['message']
+            ];
+        }
+        
+        // Sortiere nach Datum (neueste zuerst)
+        krsort($groupedCommits);
+
+        return $this->render('index/changelog.html.twig', [
+            'commits' => $groupedCommits
+        ]);
+    }
+
 }
